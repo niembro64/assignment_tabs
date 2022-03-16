95738a1867b79e44b8ac48666aff8a33ea0ef310
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-16 01:29:05 -0400

    working

--- src/App.js
+++ src/App.js
@@ -4,16 +4,40 @@ import { useState } from "react";
 import Tab from "./components/Tab";
 
 function App() {
+  const [group, setGroup] = useState([
+    { title: "t0", content: "c0" },
+    { title: "t1", content: "c1" },
+    { title: "t2", content: "c2" },
+    { title: "t3", content: "c3" },
+  ]);
+  const [bottom, setBottom] = useState("");
+
+  const onHover = (content) => {
+    // console.log(content);
+    setBottom(content);
+
+  };
+
   return (
     <div className="App">
-      <h1>Test Eric</h1>
+      <h1>Tabs Assignment</h1>
       <div id="heading">
-        <Tab />
-        <Tab />
-        <Tab />
+        {group.map((item, i) => {
+          return (
+            <div
+              key={i}
+              className="tab"
+              onMouseEnter={(e) => {
+                onHover(item.content);
+              }}
+            >
+              <Tab key={i} title={item.title} content={item.content} />
+            </div>
+          );
+        })}
       </div>
       <div id="main">
-        <p>test Shit</p>
+        <p>{bottom}</p>
       </div>
     </div>
   );
--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,29 +1,36 @@
 import { useState } from "react";
 import React, { useLayoutEffect } from "react";
 import { useEffect } from "react";
-import { render } from "@testing-library/react";
+import { getByPlaceholderText, render } from "@testing-library/react";
+import { toBePartiallyChecked } from "@testing-library/jest-dom/dist/matchers";
 
 const Tab = (props) => {
   const [title, setTitle] = useState("");
-  const [text, setText] = useState("");
+  const [content, setContent] = useState("");
   const [active, setActive] = useState(false);
 
-  const colorStyle = {
-    backgroundColor: "grey",
+  const colorActive = {
+    backgroundColor: "black",
+    color: "white",
+  };
+  const colorPassive = {
+    backgroundColor: "white",
+    color: "black",
   };
 
   const flipColor = () => {
-      setActive(!active);
-  }
+    setActive(!active);
+  };
 
   return (
     <div
       onMouseEnter={flipColor}
       onMouseLeave={flipColor}
       className="tab"
-      style={colorStyle}
+      style={active ? colorActive : colorPassive}
     >
-      {active ? "YES" : "NO"}
+      <h3>{props.title}</h3>
+      <h3>{props.content}</h3>
     </div>
   );
 };
